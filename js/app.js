a587f6d39997b4d3c79a180924d63ca5303dce78
j0t3x <jmpd1988@gmail.com>
2024-05-24 00:13:37 -0500

    chess utils

--- js/app.js
+++ js/app.js
@@ -1,13 +1,91 @@
+import {
+  algebraic_chessboard,
+  descriptive_chessboard,
+  pretty_print,
+  pretty_print_pieces,
+  representational_board,
+  starting_pieces_states
+} from './texchess.js'
+document.getElementById("descriptive").value = ""
+document.getElementById("curated").value = ""
+const replace_linebreaks = /(\r\n|\r|\n)/g;
+const get_moves = /\d{1,2}\s?(?:\w{1,3}[-xX]{1}\w{1,3}[+?!()=]{0,2}|[oO0-]{3,5})\s?(?:\w{1,3}[-xX]{1}\w{1,3}[+?!()=]{0,2}|[o0O-]{3,5})/gm;
+
 const recognize = async ({ target: { files } }) => {
+  document.getElementById("descriptive").value = ""
   document.getElementById("imgInput").src = URL.createObjectURL(files[0]);
   const worker = await Tesseract.createWorker("eng");
   const ret = await worker.recognize(files[0]);
-  console.log(ret.data.text);
+  // if (ret.data.text)
+  //   document.getElementById("descriptive").value = ret.data.text
   await worker.terminate();
 
+  recognize_vision()
 }
-const elm = document.getElementById('uploader');
-elm.addEventListener('change', recognize);
 
-const board = Chessboard2('board1', 'start')
-console.log(board.position())
+const recognize_vision = async () => {
+
+  const img = document.getElementById("imgInput");
+  const canvasElmt = document.getElementById("canvasElement");
+  const ctx = canvasElmt.getContext("2d");
+  ctx.drawImage(img, 0, 0, 500, 500 * img.height / img.width);
+
+  const response = await fetch('https://vision.googleapis.com/v1/images:annotate', {
+    method: "POST", // *GET, POST, PUT, DELETE, etc.
+    headers: {
+      "Content-Type": "application/json",
+      "x-goog-user-project": "backend-300222",
+    },
+    body: JSON.stringify({
+      "requests": [
+        {
+          "image": {
+            "content": canvasElmt.toDataURL().replace('data:image/png;base64,', '')
+
+          },
+          "features": [
+            {
+              "maxResults": 50,
+              "model": "builtin/latest",
+              "type": "DOCUMENT_TEXT_DETECTION"
+            }
+          ]
+        }
+      ]
+    }) // body data type must match "Content-Type" header
+  });
+  const res = await response.json()
+  let text = (res.responses.length > 0) ? res.responses[0].fullTextAnnotation.text : undefined
+  if (text) {
+    text = text.replaceAll(replace_linebreaks, '')
+    document.getElementById("descriptive").value = text
+  }
+}
+
+const update_curated = (event) => {
+  const text = event.target.value
+  const matches = text.matchAll(get_moves)
+  let res = ''
+  for (const match of matches) {
+    res += `${match[0]}\n`
+  }
+  document.getElementById('curated').value = res
+}
+
+const start = () => {
+  const elm = document.getElementById('uploader');
+  elm.addEventListener('change', recognize);
+
+  const descriptive_textarea = document.getElementById('descriptive');
+  descriptive_textarea.addEventListener('change', update_curated);
+
+  const board = Chessboard2('board1', 'start')
+  // console.log(board.position())
+}
+
+console.log(pretty_print(algebraic_chessboard))
+console.log(pretty_print(descriptive_chessboard))
+console.log(pretty_print_pieces(representational_board))
+console.log(starting_pieces_states)
+
+start()
---
+++ js/texchess.js
@@ -0,0 +1,143 @@
+const algebraic_y = [1, 2, 3, 4, 5, 6, 7, 8]
+const algebraic_x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+const descriptive_y = Array.from(algebraic_y)
+const descriptive_x = ['QR', 'QN', 'QB', 'Q', 'K', 'KB', 'KN', 'KR']
+const first_line_pieces = ['QRP', 'QNP', 'QBP', 'QP', 'KP', 'KBP', 'KNP', 'KRP']
+const second_line_pieces = ['QR', 'QN', 'QB', 'Q', 'K', 'KB', 'KN', 'KR']
+const representational_map_white = {
+  'P': '♙',
+  'R': '♖',
+  'N': '♘',
+  'B': '♗',
+  'Q': '♕',
+  'K': '♔',
+}
+const representational_map_black = {
+  'P': '♟',
+  'R': '♜',
+  'N': '♞',
+  'B': '♝',
+  'Q': '♛',
+  'K': '♚',
+}
+
+const build_position_map = (x, y) => {
+  const res = []
+  for (const j of Array.from(y).reverse()) {
+    for (const i of x) {
+      res.push(`${i}${j}`)
+    }
+  }
+  return res
+}
+
+const build_start_board = (whites_up = false) => {
+  const res = []
+  for (const j of Array.from(algebraic_y).reverse()) {
+    switch (j) {
+      case 8:
+        res.push(...(whites_up ? second_line_pieces.map((p) => p.concat('w')) : second_line_pieces.map((p) => p.concat('b'))))
+        break;
+      case 7:
+        res.push(...(whites_up ? first_line_pieces.map((p) => p.concat('w')) : first_line_pieces.map((p) => p.concat('b'))))
+        break;
+      case 2:
+        res.push(...(whites_up ? first_line_pieces.map((p) => p.concat('b')) : first_line_pieces.map((p) => p.concat('w'))))
+        break;
+      case 1:
+        res.push(...(whites_up ? second_line_pieces.map((p) => p.concat('b')) : second_line_pieces.map((p) => p.concat('w'))))
+      default:
+        res.push(...Array(8).fill('*'))
+        break;
+    }
+  }
+  return res
+}
+
+export const pretty_print = (board) => {
+  let res = ''
+  for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 8; i++) {
+      res += ` ${board[(j * 8) + i]}${i === 8 - 1 ? ' ' : ''}`
+    }
+    res += `\n`
+  }
+  return res
+}
+
+export const pretty_print_pieces = (board) => {
+  let res = ''
+  for (let j = 0; j < 8; j++) {
+    for (let i = 0; i < 8; i++) {
+      const data = board[(j * 8) + i]
+      if (data === '*') {
+        res += ` -${i === 8 - 1 ? ' ' : ''}`
+      } else {
+        const color = data.slice(-1)
+        const piece = data.slice(-2, -1)
+        const representational_piece = color === 'w' ? representational_map_white[piece] : representational_map_black[piece]
+        res += ` ${representational_piece}${i === 8 - 1 ? ' ' : ''}`
+      }
+    }
+    res += `\n`
+  }
+  return res
+}
+
+const notation_to_arraypos = (n) => {
+  const a = algebraic_chessboard.indexOf(n)
+  const b = descriptive_chessboard.indexOf(n)
+  return Math.max(a, b)
+}
+
+const arraypos_to_notation = (p) => {
+  return {
+    descriptive: descriptive_chessboard[p],
+    algebraic: algebraic_chessboard[p]
+  }
+}
+
+
+const build_initial_pieces_state = (whites_up = false) => {
+  const res = {}
+  for (const [index, p] of first_line_pieces.entries()) {
+    const a = arraypos_to_notation((8 - 7) * 8 + index)
+    res[p + (whites_up ? 'w' : 'b')] = {
+      arr_index: (8 - 7) * 8 + index,
+      algebraic_position: a.algebraic,
+      descriptive_posiion: a.descriptive,
+      possible_movements: []
+    }
+    const b = arraypos_to_notation((8 - 2) * 8 + index)
+    res[p + (whites_up ? 'b' : 'w')] = {
+      arr_index: (8 - 2) * 8 + index,
+      algebraic_position: b.algebraic,
+      descriptive_posiion: b.descriptive,
+      possible_movements: []
+    }
+  }
+  for (const [index, p] of second_line_pieces.entries()) {
+    const a = arraypos_to_notation((8 - 8) * 8 + index)
+    res[p + (whites_up ? 'w' : 'b')] = {
+      arr_index: (8 - 8) * 8 + index,
+      algebraic_position: a.algebraic,
+      descriptive_posiion: a.descriptive,
+      possible_movements: []
+    }
+
+    const b = arraypos_to_notation((8 - 1) * 8 + index)
+    res[p + (whites_up ? 'b' : 'w')] = {
+      arr_index: (8 - 1) * 8 + index,
+      algebraic_position: b.algebraic,
+      descriptive_posiion: b.descriptive,
+      possible_movements: []
+    }
+  }
+  return res
+}
+
+export const algebraic_chessboard = build_position_map(algebraic_x, algebraic_y)
+export const descriptive_chessboard = build_position_map(descriptive_x, descriptive_y)
+export const representational_board = build_start_board()
+export const starting_pieces_states = build_initial_pieces_state()
+
